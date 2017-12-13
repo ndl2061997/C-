a696440be187a5bd994d600f0a13990bbbb0a5d4
Nguyen Long <ndl23061997@gmail.com>
2017-12-13 22:59:31 +0700

    update

--- Graycode.cpp
+++ Graycode.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <stdlib.h>
+#include <conio.h>
 
 int intToBinary(int *b,int n){
-    int i,k;
+    int i;
     int C[100];
     i = 0;
     while ((n/2)>0){
@@ -12,22 +13,24 @@ int intToBinary(int *b,int n){
     }
     C[i] = n%2;
     i++;
-    for(int j = 0;j<=(i-1);j++){
-        k = i-j;
-        std::cout <<  k << " " ;
-        std::cout <<  C[k];
+    for(int j = 0;j<i;j++){
+        b[j] = C[i-j-1];
     }
 }
 
-void BinarytoGray(int b[], int n){
-    int i;
-    while (i<n & b[i]==0)
+void BinarytoGray(int *b, int n){
+    int i = n-1;
+    while (i>0)
     {
-        i++;
-    }
-    if(i<n){
-        if(b[i] == 0) b[i] = 1;
-        if(b[i] == 1) b[i] = 0;
+        if(b[i-1] == 1 ){
+            if(b[i] == 0){
+                b[i] = 1;
+                i--;
+            }else{
+                b[i]=0;
+                i--;
+            }
+        } else i--;
     }
 }
 
@@ -37,9 +40,15 @@ int main(){
     std::cout << "Nhap so : ";
     std::cin >> n;
     i = intToBinary(B,n);
-    //BinarytoGray(B,i);
-    std::cout << "Ma gray :";
+    std::cout << "Binary : ";
     for(int j = 0;j<i;j++){
         std::cout <<  B[j];
     }
+    std::cout << '\n' <<"Gray :   ";
+    BinarytoGray(B,i);
+    for(int j = 0;j<i;j++){
+        std::cout <<  B[j];
+    }
+    getch();
+    return 0;
 }
